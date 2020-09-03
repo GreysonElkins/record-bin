bb010531fb97f00ab9a528037e7c291393aeda11
Greyson Elkins <greysonelkins@gmail.com>
2020-09-02 18:02:42 -0600

    First successful take-off for server

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 
-const server = express()
+const app = express()
 
-server.set('port', process.env.PORT || 4200)
-server.locals.records = []
-apps.locals.title = 'The Record Bin'
+app.set('port', process.env.PORT || 4200)
+app.locals.records = []
+app.locals.title = 'The Record Bin'
 
-server.get('/', (request, response) => {
+app.get('/', (request, response) => {
   response.send(apps.locals.records)
 })
 
-server.listen(server.get('port'), () => {
-  console.log(`Greyson's server, ${apps.locals.title}, 
-    is running on http://localhost:${server.get('port')}, 
+app.listen(app.get('port'), () => {
+  console.log(`Greyson's app, ${app.locals.title}, 
+    is running on http://localhost:${app.get('port')}, 
     and it's pretty much the greatest thing ever.`
   )
 })
